f25459b8ccd4cf4f1cdf1030e29d61790a1826b1
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-09-17 13:45:45 -0300

    chore: import test services in test controllers

--- src/controllers/testControllers.ts
+++ src/controllers/testControllers.ts
@@ -1,10 +1,11 @@
 import { Tests } from '@prisma/client';
 import { Request, Response } from 'express';
 import { TTestData } from '../types/testType';
+import * as testService from '../services/testServices';
 
 export async function createTest(req: Request, res: Response) {
   const newTest: TTestData = req.body;
 
-  const test: Tests = '';
+  const test: Tests = await testService.createTest(newTest);
   res.status(200).send(`Test: ${test.name}, registered successfully!`);
 }
