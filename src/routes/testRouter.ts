4c565a900df5bd10f402fb2bb3dff1ecfd723399
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-09-17 14:48:01 -0300

    chore: create get tests by teachers route

--- src/routes/testRouter.ts
+++ src/routes/testRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTest, getTestsByDiscipline } from "../controllers/testControllers";
+import { createTest, getTestsByDiscipline, getTestsByTeachers } from "../controllers/testControllers";
 import { authenticateUser } from "../middlewares/authenticateUserMiddleware";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware";
 import testSchema from "../schemas/testSchema";
@@ -8,6 +8,7 @@ import testSchema from "../schemas/testSchema";
 const testRouter = Router();
 
 testRouter.post('/test', authenticateUser, validateSchemaMiddleware(testSchema), createTest);
-testRouter.get('/tests', authenticateUser, getTestsByDiscipline);
+testRouter.get('/tests/disciplines', authenticateUser, getTestsByDiscipline);
+testRouter.get('/tests/teachers', authenticateUser, getTestsByTeachers)
 
 export default testRouter;
