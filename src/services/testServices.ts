5292b3548c8d21c7c09ab89ae39e55ab25e2b0d4
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-09-17 13:44:53 -0300

    chore: create testServices

---
+++ src/services/testServices.ts
@@ -0,0 +1,8 @@
+import { Tests } from "@prisma/client";
+import { TTestData } from "../types/testType";
+
+export async function createTest(testData: TTestData) {
+  const test: Tests = ;
+
+  return test;
+}
