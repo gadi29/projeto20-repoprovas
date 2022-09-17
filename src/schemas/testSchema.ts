abcb5de3f12f2fb169d2ef7a80e3664702f122ba
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-09-17 12:12:18 -0300

    import test type in test schema

--- src/schemas/testSchema.ts
+++ src/schemas/testSchema.ts
@@ -1,6 +1,7 @@
 import Joi from "joi";
+import { TTestData } from "../types/testType";
 
-const testSchema = Joi.object({
+const testSchema = Joi.object<TTestData>({
   name: Joi.string().trim().required(),
   pdfUrl: Joi.string().uri().required()
 })
