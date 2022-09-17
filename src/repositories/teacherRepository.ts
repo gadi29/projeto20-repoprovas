4f23d24a9a88b22e742653d52392d3378eb58d9e
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-09-17 15:00:58 -0300

    chore: create getTestsByTeachers

--- src/repositories/teacherRepository.ts
+++ src/repositories/teacherRepository.ts
@@ -1,5 +1,31 @@
 import prisma from "../config/database";
 
 export async function getTests() {
+  const tests = await prisma.teachers.findMany({
+    select: {
+      name: true,
+      TeachersDisciplines: {
+        select: {
+          discipline: {
+            select: {
+              name: true,
+            }
+          },
+          Tests: {
+            select: {
+              name: true,
+              pdfUrl: true,
+              category: {
+                select: {
+                  name: true,
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  })
 
+  return tests;
 }
