ce15215d240af61402c9627b0b0d7cd63ab16e19
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-09-17 15:35:33 -0300

    chore: confirm teacher discipline to create test

--- src/services/testServices.ts
+++ src/services/testServices.ts
@@ -1,13 +1,19 @@
-import { Categories, Tests } from "@prisma/client";
+import { Categories, TeachersDisciplines, Tests } from "@prisma/client";
 import { TTestData } from "../types/testType";
+
 import * as categoryServices from './categoryServices';
+import * as teacherDisciplineServices from './teacherDisciplineServices';
 import * as teacherServices from './teacherServices';
 import * as termServices from './termServices';
+
 import * as testRepository from '../repositories/testRepository';
 
 export async function createTest(testData: TTestData) {
   const category: Categories = await categoryServices.getCategoryById(testData.categoryId);
   if (!category) throw { type: 'not_found', message: 'Category not found' };
+
+  const teacherDiscipline: TeachersDisciplines = await teacherDisciplineServices.getTeacherDisciplineById(testData.teacherDisciplineId);
+  if (!teacherDiscipline) throw { type: 'not_found', message: 'Teacher or/and discipline not found' };
 
   const test: Tests = await testRepository.createTest(testData);
 
