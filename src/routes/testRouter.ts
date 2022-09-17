2f1a906d269bcc6b6a0c499235cb43326f0a860e
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-09-17 13:22:23 -0300

    chore: import test schema in test router

--- src/routes/testRouter.ts
+++ src/routes/testRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticateUser } from "../middlewares/authenticateUserMiddleware";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware";
+import testSchema from "../schemas/testSchema";
 
 
 const testRouter = Router();
 
-testRouter.post('/test', authenticateUser, validateSchemaMiddleware(),);
+testRouter.post('/test', authenticateUser, validateSchemaMiddleware(testSchema),);
 
 export default testRouter;
